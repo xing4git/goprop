79f341cc5e8a3a2b90a1b6747abc6f3db481c14b
xing4git <xing.zhang.programming@gmail.com>
2012-09-14 19:41:13 +0800

    add note

--- prop.go
+++ prop.go
@@ -15,11 +15,11 @@ import (
 /**
  * Load loads properties from propPath
  */
-func Load(propPath string) (prop map[string]string, err error) {
-	prop = make(map[string]string)
+func Load(propPath string) (map[string]string, error) {
+	prop := make(map[string]string)
 	file, err := os.Open(propPath)
 	if err != nil {
-		return
+		return nil, err
 	}
 	defer file.Close()
 
@@ -27,37 +27,47 @@ func Load(propPath string) (prop map[string]string, err error) {
 	reader := bufio.NewReader(file)
 	for {
 		count++
+
 		linebytes, isPrefix, err := reader.ReadLine()
 		if err != nil {
 			if err == io.EOF {
-				err = nil
+				return prop, nil
 			}
-			break
+			return nil, err
 		}
 		if isPrefix {
-			err = errors.New("contains too long line")
-			break
+			err = errors.New("contains too long line at line " + strconv.Itoa(count))
+			return nil, err
 		}
+
+		// check line
 		line := string(linebytes)
 		line = strings.TrimSpace(line)
-		if strings.HasPrefix(line, "#") {
+		if line == "" || strings.HasPrefix(line, "#") {
 			continue
 		}
 		equalNum := strings.Count(line, "=")
 		if equalNum != 1 {
-			err = errors.New("invalid format at line " + strconv.Itoa(equalNum))
-			break
+			err = errors.New("invalid format at line " + strconv.Itoa(count))
+			return nil, err
 		}
+
+		// check key and value
 		equalIndex := strings.Index(line, "=")
 		key := strings.TrimSpace(string(line[0:equalIndex]))
 		value := strings.TrimSpace(string(line[equalIndex+1:]))
 		if key == "" || value == "" {
-			err = errors.New("invalid format at line " + strconv.Itoa(equalNum))
+			err = errors.New("invalid format at line " + strconv.Itoa(count))
+			return nil, err
 		}
+
+		// check duplicate key
 		if _, ok := prop[key]; ok {
 			err = errors.New("contains duplicate key: " + key)
+			return nil, err
 		}
+
 		prop[key] = value
 	}
-	return
+	return prop, nil
 }
